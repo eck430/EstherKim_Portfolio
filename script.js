3cbb1a05041a3f294934dde16745db82cd72f45c
eck430 <54334107+eck430@users.noreply.github.com>
2024-07-20 17:56:34 -0400

    layout and navbar

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('hamburger').addEventListener('click', () => {
+    document.getElementById('hamburger').classList.toggle('active');
+    document.getElementById('nav_list').classList.toggle('active');
+});
